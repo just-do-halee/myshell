cc32dfb0a11d5f400bade771e66d2eec3828c79f
just-do-halee <just.do.halee@gmail.com>
2021-09-13 14:12:22 +0900

    3.  node  [update]

---
+++ node/core.sh
@@ -0,0 +1,8 @@
+alias ys="yarn start"
+alias yd="yarn dev"
+alias yb="yarn build"
+alias yadd="yarn add $@"
+alias ns="npm start"
+alias nd="npm run dev"
+alias nb="npm run build"
+alias ni="npm install $@"
