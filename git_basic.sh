99305e89bac8802e4a7e97ae7806d11be69eefc6
just-do-halee <just.do.halee@gmail.com>
2021-09-16 08:10:04 +0900

    6.  git: g3_re  [update]

--- git_basic.sh
+++ git_basic.sh
@@ -4,6 +4,13 @@ function g3() {
         git push origin main
 }
 
+function g3_re() {
+        git reset HEAD^
+        git add .
+        git commit -m "$@"
+        git push -f origin main
+}
+
 function gt() {
         s="$@"
         git tag -a "$@" -m "${s}"
