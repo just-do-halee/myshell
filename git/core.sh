e3e0fd97e7c1708d5c1e7f49f7545ad0a17f7611
just-do-halee <just.do.halee@gmail.com>
2021-10-02 01:31:02 +0900

    8.  git from & new  [update]

--- git/core.sh
+++ git/core.sh
@@ -1,3 +1,15 @@
+function gnew() {
+        mkdir "$1"
+        cd "$1"
+        git init
+        git remote add origin "$2"
+}
+
+function gfrom() {
+        git clone "$@"
+        ls
+}
+
 function gr() {
         git remote add "$@"
 }
