8d875d49548d2e4250ff8c4c4ac53326f04ed710
just-do-halee <just.do.halee@gmail.com>
2021-12-01 07:47:03 +0900

    18.  rust: remove nocapture  [fixed]

--- rust/core.sh
+++ rust/core.sh
@@ -1,9 +1,9 @@
 alias r="cargo run"
-alias t="cargo test -- --nocapture"
+alias t="cargo test"
 alias rr="cargo run -p $@"
 
 function tt() {
-        cargo test -p "$@" -- --nocapture
+        cargo test -p "$@"
 }
 
 alias cbd="cargo clean; cargo test; cargo build; cargo build --release; cargo doc --open "$@""
